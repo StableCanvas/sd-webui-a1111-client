57d596fe2030bc05eb1ab9dd953dd02bc2c939dd
zhzluke96 <zhz961101@gmail.com>
2023-08-30 16:17:40 +0800

    :fire:remove generate code

--- src/client/models/ResetBody.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ResetBody = {
-    session_hash: string;
-    fn_index: number;
-};
--- src/client/models/SDVaeItem.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type SDVaeItem = {
-    model_name: string;
-    filename: string;
-};
